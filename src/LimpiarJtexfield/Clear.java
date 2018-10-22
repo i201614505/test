adbae99283d1930783837d482ac3294232d05979
Dev01 <Dev01@SOSNet01>
2018-10-22 10:32:39 -0500

    ClearJtexfield

---
+++ src/LimpiarJtexfield/Clear.java
@@ -0,0 +1,16 @@
+package LimpiarJtexfield;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Clear {
+
+	public static void ClearJtexfield(JPanel panel) {
+		for(int i = 0; panel.getComponents().length > i; i++){
+			if(panel.getComponents()[i] instanceof JTextField){
+                ((JTextField)panel.getComponents()[i]).setText("");
+            }
+	    }
+	}
+
+}
