a0769cae077b5c5576e292941b47b5e350697d85
Dev01 <Dev01@SOSNet01>
2018-10-22 11:55:57 -0500

    act

--- src/LimpiarJtexfield/Clear.java
+++ src/LimpiarJtexfield/Clear.java
@@ -8,7 +8,7 @@ public class Clear {
 	public static void ClearJtexfield(JPanel panel) {
 		for(int i = 0; panel.getComponents().length > i; i++){
 			if(panel.getComponents()[i] instanceof JTextField){
-                ((JTextField)panel.getComponents()[i]).setText("");
+                ((JTextField)panel.getComponents()[i]).setText("0.0");
             }
 	    }
 	}
